4e3d8664757a8810f1d4ebebab73169a6134679e
willofdaedalus <manny.is.daedalus@pm.me>
2025-04-05 19:35:27 +0000

    new: parser types for use across the parser

---
+++ internal/engine/parser/parser_types.go
@@ -0,0 +1,76 @@
+package parser
+
+import "willofdaedalus/mime/internal/engine/lexer"
+
+type (
+	dataType   int
+	constraint int
+)
+
+const (
+	dataText dataType = iota + 1
+	dataInt
+	dataReal
+	dataTimestamp
+	dataUUID
+)
+
+const (
+	consUnique constraint = iota + 1
+	consIncrement
+	consPrimary
+	consRequired
+	consFK
+)
+
+var enumerableTypes = map[lexer.TokenType]struct{}{
+	lexer.TokenInt:    {},
+	lexer.TokenString: {},
+}
+
+var typeConstraintMap = map[dataType][]constraint{
+	dataText: {
+		consUnique, consPrimary, consFK,
+	},
+	dataInt: {
+		consUnique, consIncrement, consPrimary, consFK,
+	},
+	dataReal: {
+		consUnique, consIncrement, consRequired,
+	},
+	dataUUID: {
+		consRequired,
+	},
+}
+
+func (d dataType) string() string {
+	switch d {
+	case dataText:
+		return "text"
+	case dataInt:
+		return "int"
+	case dataReal:
+		return "real"
+	case dataTimestamp:
+		return "timestamp"
+	case dataUUID:
+		return "uuid"
+	}
+	return "not checked!"
+}
+
+func (c constraint) string() string {
+	switch c {
+	case consUnique:
+		return "unique"
+	case consIncrement:
+		return "increment"
+	case consPrimary:
+		return "primary"
+	case consRequired:
+		return "required"
+	case consFK:
+		return "fk"
+	}
+	return "not checked!"
+}
